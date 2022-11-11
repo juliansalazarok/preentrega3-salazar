f234911749879b3b496ee95cd82ebc728710bea0
julian salazar <julian456@gmail.com>
2022-11-11 17:27:04 -0300

    Pre entrega 3

--- preentrega3.js
+++ preentrega3.js
@@ -1,76 +1,6 @@
-// Calculador de precio final de estadía para proyecto de alojamientos turísticos.
-
-/* 
-Se pedirá al usuario: 
-- alojamiento deseado
-- cantidad de noches
-- cantidad de huéspedes.
-
-
-El sistema devolverá: 
-- valor por noche
-- costo total de la estadía
-*/
-
-
-// Mensaje inicial por consola
-//console.log("Calcule el valor de su estadía"); 
-
-// Constantes y función para limpiar strings y devolver numbers
-const precio_serrano = 50;
-const precio_santelmo = 70;
-const precio_bellini = 95;
-
-function precio_depto(depto){
-
-    if(depto == "Serrano" || depto == "serrano"){
-        return precio_serrano;
-    }
-    else if(depto == "San Telmo" || depto == "san telmo"){
-        return precio_santelmo;
-    }
-    else if(depto == "Bellini" || depto == "bellini"){
-        return precio_bellini;
-    }
-    else{
-        return
-    }
-}
-
-// Variables para pedir info de entrada al usuario
-//let alojamiento = prompt("Ingrese el alojamiento deseado: Serrano / San Telmo / Bellini");
-//let cant_noches = parseInt(prompt("Cuántas noches desea hospedarse?"));
-//let cant_huespedes = parseInt(prompt("Cuántas personas se hospedarán? Sin contar menores de 2 años"));
-
-// Función que genera el cálculo deseado
-/*function calcular_estadia(alojamiento , cant_noches , cant_huespedes){
-
-    let preciofinal
-    if(cant_huespedes <= 2){
-        preciofinal = precio_depto(alojamiento);
-    }
-    else if(cant_huespedes === 3){
-        preciofinal = precio_depto(alojamiento)* 1.10;
-    }
-    else {
-        console.log("El máximo de huéspedes que aceptan nuestros alojamientos es de 3 adultos");
-    }
-
-    preciofinal = preciofinal * cant_noches;
-    
-    return preciofinal
-
-}
-
-
-
-// Salida por console.log 
-    console.log("El valor por noche en " , alojamiento , " es de " , precio_depto(alojamiento));
-    console.log("El valor total de su estadía es de: " , calcular_estadia(alojamiento, cant_noches , cant_huespedes));
-
-*/
 
 let arreglo_estadia = [];
+let estadia_ls = "estadia";
 
 function calcular_estadia(){
 
@@ -84,22 +14,25 @@ function calcular_estadia(){
 
 
     let arreglo_JSON = JSON.stringify(arreglo_estadia);
-    localStorage.setItem("estadía" , arreglo_JSON);
+    localStorage.setItem(estadia_ls , arreglo_JSON);
 
-    mostrar_estadia(recuperando);
+    getEstadia();
 
 }
 
-let recuperando = JSON.parse(localStorage.getItem("estadía"));
-
-if(recuperando && recuperando.length){
+function getEstadia(){
     
-    recuperando.forEach(respuesta=>mostrar_estadia(respuesta));
+    let recuperando = JSON.parse(localStorage.getItem(estadia_ls));
+    
+    if(recuperando){
+        mostrar_estadia(recuperando[recuperando.length-1])
+
+    }
+
 }
 
 
 function mostrar_estadia(recuperando){
-    console.log(recuperando);
     let fila = document.createElement("tr");
     fila.innerHTML = `<td>${recuperando.noches}</td>
                     <td>${recuperando.alojamiento}</td>
